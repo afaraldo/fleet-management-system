f5b900ef2f4ef7ec50d8ea635e2c54b817d7b5c9
afaraldo <iceberg.04@gmail.com>
2022-09-16 23:39:43 -0400

    Add SystemSetting gem Add new color style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 //import "controllers"
 import * as bootstrap from "bootstrap"
+
+// Import JQuery
+//import 'add_jquery'
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
