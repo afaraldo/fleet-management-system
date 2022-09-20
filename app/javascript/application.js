89749421481ff6433643f18e3eefdf2df4748329
afaraldo <iceberg.04@gmail.com>
2022-09-20 17:31:02 -0400

    add polipop library js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,16 +2,32 @@
 import "@hotwired/turbo-rails"
 //import "controllers"
 import * as bootstrap from "bootstrap"
-import "polipop/dist/polipop.min"
-import Polipop from 'polipop';
 
 // Import JQuery
-import jquery from 'jquery'
+//import jquery from 'jquery'
+
+import jquery from "jquery";
+import Polipop from 'polipop';
+
 window.jQuery = jquery
 window.$ = jquery
+window.Polipop = Polipop
+var polipop1 = new Polipop('lollipop', {
+    layout: 'popups',
+    insert: 'before',
+    pool: 5,
+    sticky: true,
+    theme: 'default',
+    closer: false,
+    position: 'top-right'
+});
+window.polipop1 = polipop1
 
 $( document ).ready(function() {
     $('.layout-menu-toggle').click(function(){
         $('#html-document').toggleClass('layout-menu-expanded');
     });
 });
+
+
+
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,9 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+polipop1.add({
+    content: 'This is the message content.',
+    title: 'Message',
+    type: 'success',
+});
