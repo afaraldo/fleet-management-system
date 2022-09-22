a49e0ebbc5fa080be6cb6c7171337de0270f5aca
KariSanabria <karina.sanabria75@gmail.com>
2022-09-21 22:59:16 -0300

    Small changes to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   #get 'login/index'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-
-  # get 'login/index'
-  # get 'users/index'
-  # get 'dash_board/index'
-=======
   mount SystemSettings::Engine, at: '/system_settings'
 
   get 'dash_board/index'
->>>>>>> main
   resources :cars
   resources :work_orders
   resources :employees
