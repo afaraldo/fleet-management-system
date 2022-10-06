5459d535289ce29a2c068f45f4c91c70e08cf284
afaraldo <iceberg.04@gmail.com>
2022-10-06 04:30:31 -0300

    #FMS-4 add new migrate to work_orders

---
+++ db/migrate/20221006055921_rename_column_to_work_orders.rb
@@ -0,0 +1,22 @@
+class RenameColumnToWorkOrders < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :work_orders
+    create_table :work_orders do |t|
+      t.date :date, null: false
+      t.bigint :number, null: false
+      t.string :description, null: false
+      t.string :city
+      t.integer :workdays
+      t.integer :start_mileage
+      t.integer :final_mileage
+      t.references :employee
+
+      t.timestamps
+    end
+
+    add_index :work_orders, :date
+    add_index :work_orders, :number
+    add_index :work_orders, :description
+    add_index :work_orders, :city
+  end
+end
