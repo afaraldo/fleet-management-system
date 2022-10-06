09cb6ea028cc188f9648aded570cdcf8bb7fc8e8
afaraldo <iceberg.04@gmail.com>
2022-10-06 15:02:11 -0300

    #FMS-4 remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   mount SystemSettings::Engine, at: '/system_settings'
 
   resources :dash_board, only: [:index]
-  resources :cars
-  resources :employees
-  resources :users
-  resources :work_orders
+  resources :cars, except: [:show]
+  resources :employees, except: [:show]
+  resources :users, except: [:show]
+  resources :work_orders, except: [:show]
 
   # Defines the root path route ("/")
   root 'dash_board#index'
