f78f5c0c320d2dddc93d690fff83e5229f09eb20
afaraldo <iceberg.04@gmail.com>
2022-10-08 03:12:31 -0300

    Fix user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of :email }
-    it { should validate_presence_of :encrypted_password }
+
   end
 end
