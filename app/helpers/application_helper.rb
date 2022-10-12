e13feca75879991848368b48fa1267517c29ed74
afaraldo <iceberg.04@gmail.com>
2022-10-12 15:25:31 -0300

    Fix rubocop warnings
    https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Style/ParenthesesAroundCondition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 # Helper for Application
 module ApplicationHelper
   def generate_model_for_url(model)
-    if (model.class.superclass != ApplicationRecord)
-      model.becomes(model.class.superclass)
-    else
+    if model.class.superclass == ApplicationRecord
       model
+    else
+      model.becomes(model.class.superclass)
     end
   end
 end
