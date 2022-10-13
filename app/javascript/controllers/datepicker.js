8c0cf057a3fa5f88338ede13c669538dd24ff142
afaraldo <iceberg.04@gmail.com>
2022-10-13 02:51:32 -0300

    FMS-10 add last number in work order form change input places in form

--- app/javascript/controllers/datepicker.js
+++ app/javascript/controllers/datepicker.js
@@ -3,5 +3,17 @@ import { Controller } from "stimulus"
 export default class extends Controller {
     static targets = [ "input", "toggle" ]
 
-    //datepicker
+    connect() {
+        //global options
+        this.config = {
+            ...this.config, //spread options in case some where defined in initialize
+            enableTime: true,
+            time_24hr: true
+        };
+
+        //always call super.connect()
+        super.connect();
+    }
+
+    datepicker
 }
