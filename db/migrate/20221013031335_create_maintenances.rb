876fbc41b920b59e09c5921407c92b4a0624a0b6
KariSanabria <karina.sanabria75@gmail.com>
2022-10-15 17:06:20 -0300

    null: false for migrate maintenance

--- db/migrate/20221013031335_create_maintenances.rb
+++ db/migrate/20221013031335_create_maintenances.rb
@@ -1,7 +1,7 @@
 class CreateMaintenances < ActiveRecord::Migration[7.0]
   def change
     create_table :maintenances do |t|
-      t.date :date
+      t.date :date, null: false
       t.references :mechanical_workshop, null: false, foreign_key: true
       t.integer :budget
       t.references :car, null: false, foreign_key: true
