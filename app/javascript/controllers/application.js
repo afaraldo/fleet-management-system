c94492ac48c513116351d844bad6b26f00c76b43
afaraldo <iceberg.04@gmail.com>
2022-10-17 02:48:38 -0300

    try add flatpickr

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,14 +1,16 @@
 import { Application } from "@hotwired/stimulus"
-import Datepicker from 'stimulus-datepicker'
 
 const application = Application.start()
-application.register('datepicker', Datepicker)
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
+Stimulus.register("stimulus", Stimulus)
+// import Flatpickr
+//import Flatpickr from "stimulus-flatpickr";
 
 export { application }
-
-
-
