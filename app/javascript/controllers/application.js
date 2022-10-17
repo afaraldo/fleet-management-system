d02e07ad23889ad880e40caed16dcd6f351135b9
afaraldo <iceberg.04@gmail.com>
2022-10-17 03:24:39 -0300

    fix js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,9 +7,9 @@ application.debug = false
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
-Stimulus.register("stimulus", Stimulus)
+//const context = require.context("./controllers", true, /\.js$/)
+//Stimulus.load(definitionsFromContext(context))
+//Stimulus.register("stimulus", Stimulus)
 // import Flatpickr
 //import Flatpickr from "stimulus-flatpickr";
 
