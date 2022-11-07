a84c62a0441e802d5ddf2bc8f18de4870cd1e56d
afaraldo <iceberg.04@gmail.com>
2022-11-07 20:12:19 -0300

    FMS-24 Add area column to work_orders

--- db/migrate/20221105220748_add_index_unique_number_to_work_orders.rb
+++ db/migrate/20221105220748_add_index_unique_number_to_work_orders.rb
@@ -1,11 +1,14 @@
 # In this migration add indexes
 class AddIndexUniqueNumberToWorkOrders < ActiveRecord::Migration[7.0]
   def change
+    change_table :work_orders, bulk: true do |t|
+      t.remove :workdays, type: :integer
+      t.string :area, null: false
+    end
     remove_index :work_orders, :number
     add_index :work_orders, :number, unique: true, if_not_exists: true
     add_index :work_orders, :start_date, if_not_exists: true
     add_index :work_orders, :final_date, if_not_exists: true
     add_index :work_orders, :status, if_not_exists: true
-    remove_column :work_orders, :workdays, :integer
   end
 end
