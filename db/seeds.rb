d729c72e2d5681284333c2ea44fad4f5703fa5b3
afaraldo <iceberg.04@gmail.com>
2022-11-11 20:43:04 -0300

    FMS-15 fix rubocop warnings

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 PaperTrail.request(whodunnit: 'Administrador') do
-
   superuser = User.find_or_create_by(email: 'admin@email.com')
   superuser.update(password: 'test123')
 
