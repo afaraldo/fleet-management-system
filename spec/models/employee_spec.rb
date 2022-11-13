2223eea63fdccd227f7c42990c21f674393bffeb
KariSanabria <karina.sanabria75@gmail.com>
2022-11-13 13:16:14 -0300

    Little changes.

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Employee, type: :model do
     it { should have_many(:work_orders) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:document) }
+  end
+
   describe '.to_s' do
     subject { create(:employee) }
     context 'should return a String composed of name and last_name' do
