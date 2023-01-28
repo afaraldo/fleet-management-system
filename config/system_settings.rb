cbd73b43786d56439f0dc8d41be200a4011f676c
KariSanabria <karina.sanabria75@gmail.com>
2023-01-28 15:50:27 -0300

    Small changes on the dashboard.

--- config/system_settings.rb
+++ config/system_settings.rb
@@ -4,5 +4,5 @@ string :site_name, value: 'Gobernación de Itapúa', description: 'This name app
 string :logo_file, value: 'logo.png'
 string :user_file, value: 'user.png'
 string :favicon, value: 'favicon.ico'
-string :success, value: 'success.png'
-string :cars, value: 'cars.png'
+string :icono_dashboard, value: 'icono_dashboard.png'
+string :logo_dashboard, value: 'logo_dashboard.png'
