ed58696f7affbeffb860513b2a0ba43bfcfcdfcb
Adrian Aguero <iceberg.04@gmail.com>
2023-05-26 02:07:26 -0400

    feat: add reset password feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable
+  devise :database_authenticatable, :recoverable
 
   has_one_attached :avatar # Active Storage
   has_many :notifications, as: :recipient, dependent: :destroy # https://github.com/excid3/noticed
