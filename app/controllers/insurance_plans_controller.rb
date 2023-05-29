ba0573597011b945ef23c852318975ca8385a498
KariSanabria <karina.sanabria75@gmail.com>
2023-05-29 00:00:29 -0300

    change in controller..

--- app/controllers/insurance_plans_controller.rb
+++ app/controllers/insurance_plans_controller.rb
@@ -3,6 +3,6 @@ class InsurancePlansController < ApplicationController
   resource :insurance_plan
 
   def model_params
-    params.require(:insurance_plan).permit(:contract_date, :expiry_date, :amount, :insurance_carrier_id, car_ids: [])
+    params.require(:insurance_plan).permit(:contract_date, :expiry_date, :type_coverage, :amount, :insurance_carrier_id, car_ids: [])
   end
 end
