6fc455c12425fb9f171e30661a4c0da5fc90fd11
KariSanabria <karina.sanabria75@gmail.com>
2023-06-05 00:26:35 -0300

    little changes.

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -3,4 +3,8 @@ class Repair < ApplicationRecord
   belongs_to :car
   belongs_to :mechanical_workshop
   delegate :name, to: :mechanical_workshop, prefix: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[car_id created_at date mechanical_workshop_id updated_at]
+  end
 end
