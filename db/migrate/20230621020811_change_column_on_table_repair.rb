6821d647537dd6cecd6c54a11bb40f8c5bde502a
KariSanabria <karina.sanabria75@gmail.com>
2023-06-20 23:40:57 -0300

    ..

--- db/migrate/20230621020811_change_column_on_table_repair.rb
+++ db/migrate/20230621020811_change_column_on_table_repair.rb
@@ -1,3 +1,4 @@
+# This migration changes the date column to not null in repairs
 class ChangeColumnOnTableRepair < ActiveRecord::Migration[7.0]
   def change
     change_column_null :repairs, :date, false
