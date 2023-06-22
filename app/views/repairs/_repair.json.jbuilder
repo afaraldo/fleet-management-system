d2bfd339b9a27401319a9e91ebdc631bab676cc6
KariSanabria <karina.sanabria75@gmail.com>
2023-06-22 10:14:28 -0300

    Fms 44 abm repair (#44)
    * Repair ABM scaffolding..
    
    * little changes.
    
    * test.
    
    * little changes.
    
    * test changes.
    
    * changes in form.
    
    * access changes.
    
    * migration changes.
    
    * ..

---
+++ app/views/repairs/_repair.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! repair, :id, :date, :car_id, :mechanical_workshop_id, :repairs, :created_at, :updated_at
+json.url repair_url(repair, format: :json)
---
+++ db/migrate/20230531034003_create_repairs.rb
@@ -0,0 +1,13 @@
+# Migration for Repairs
+class CreateRepairs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :repairs do |t|
+      t.date :date
+      t.references :car, null: false, foreign_key: true
+      t.references :mechanical_workshop, null: false, foreign_key: true
+      t.string :repairs
+
+      t.timestamps
+    end
+  end
+end
