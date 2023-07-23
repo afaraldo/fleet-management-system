04c80fb787db07c1324fa85208be35f9b6fb3c29
afaraldo <iceberg.04@gmail.com>
2023-07-22 20:41:13 -0400

    feat: add show/hide password input in login

---
+++ app/javascript/controllers/login_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "stimulus";
+
+// Connects to data-controller="login"
+export default class extends Controller {
+  static targets = ["passwordInput", "passwordIcon"];
+
+  connect() {
+    console.log("Login controller connected")
+  }
+
+  passwordToggle() {
+    const passwordInput = this.passwordInputTarget;
+    const passwordIcon = this.passwordIconTarget;
+
+    if (passwordInput.type === 'text') {
+      passwordInput.type = 'password'
+      passwordIcon.classList.replace("bx-show", "bx-hide")
+    } else {
+      passwordInput.type = 'text'
+      passwordIcon.classList.replace("bx-hide", "bx-show")
+    }
+  }
+
+
+}
