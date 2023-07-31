bd4f34bfa53398a2d78bac14c3f9f271bd836270
afaraldo <iceberg.04@gmail.com>
2023-07-30 23:26:49 -0400

    feat: add importer for cars and employees

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module FleetManagementSystem
 
     config.i18n.default_locale = :es
     config.i18n.load_path += SimpleFormRansack.locale_files # Using for https://github.com/kaspernj/simple_form_ransack
-    config.autoload_paths += %W[#{config.root}/app/policies]
+    config.autoload_paths += %W[#{config.root}/app/policies, #{config.root}/lib]
   end
 end
