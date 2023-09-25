5e60a8739459d7057b0ac4d6118f4f43c511b876
afaraldo <iceberg.04@gmail.com>
2023-09-25 02:37:05 -0400

    fix: show car, settings, etc

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,7 +4,7 @@ class CarsController < ApplicationController
 
   def show
     add_breadcrumb I18n.t("activerecord.models.#{resource_name}.other"), polymorphic_url(plural_resource_name, params: session[controller_name], only_path: true) # Use for breadcrumbs_on_rails gem
-    add_breadcrumb I18n.t('buttons.show'), nil # Use for breadcrumbs_on_rails gem
+    add_breadcrumb @car.plate_number, nil # Use for breadcrumbs_on_rails gem
     find_resource
     @work_orders = @car.work_orders
   end
