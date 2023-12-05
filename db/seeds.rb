68d5fa060d1edb4802a64c9856ce015f83816e4c
afaraldo <iceberg.04@gmail.com>
2023-12-04 22:38:42 -0300

    fix: logo organization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-PaperTrail.request(whodunnit: 'iceberg.04+gobernacion-itapua@gmail.com') do
+PaperTrail.request(whodunnit: 'iceberg.04@gmail.com') do
   organization_names = ['Gobernación Itapúa', 'Municipalidad de Cambyreta']
   organization_names.each do |name|
     organization = Organization.find_or_create_by!(name:)
